70aa64a7fdce5b4983f090c1d43fed75b728db5c
Vaishnavi Shetti <vaishnavishetti2004@gmail.com>
2025-08-19 14:22:36 +0530

    Image issue resolved

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -17,9 +17,10 @@ router.post("/upload-image",upload.single("image"),(req,res)=>{
     if(!req.file){
         return res.status(400).json({message:"No file uploaded"});
     }
-    const imageUrl=`${req.protocol}://${req.get("host")}/uploads/${
-        req.file.filename
-    }`;
+    // const imageUrl=`${req.protocol}://${req.get("host")}/uploads/${
+    //     req.file.filename
+    // }`;
+    const imageUrl = `${process.env.API_BASE_URL}/uploads/${req.file.filename}`;
     res.status(200).json({imageUrl});
 });
 
--- frontend/expense-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/layouts/SideMenu.jsx
@@ -25,10 +25,10 @@ const SideMenu = ({activeMenu}) => {
   return <div className='w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20'>
     <div className='flex flex-col items-center justify-center gap-3 mt-3 mb-7'>
         {user?.profileImageUrl ?(
-            <img
-                src={user?.profileImageUrl || ""}
-                alt="Profile Image"
-                className='w-20 h-20 bg-slate-400 rounded-full'
+                <img
+                    src={user?.profileImageUrl.replace("http://localhost:8000", import.meta.env.VITE_API_URL) || ""}
+                    alt="Profile Image"
+                    className='w-20 h-20 bg-slate-400 rounded-full'
                 />):(
                     <CharAvatar
                     fullName={user?.fullName}
